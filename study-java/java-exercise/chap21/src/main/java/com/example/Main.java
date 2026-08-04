781b682ebc45e054b632e19d997dd44ac476f62c
yusay1498 <u.yusay1498@gmail.com>
2026-08-04 15:22:31 +0900

    Add chap21 HealthFood and DrugStore stream exercise

--- study-java/java-exercise/chap21/src/main/java/com/example/Main.java
+++ study-java/java-exercise/chap21/src/main/java/com/example/Main.java
@@ -1,8 +1,24 @@
 package com.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, This is exercise.");
+        List<HealthFood> healthFoodList = new HealthFood(
+                "dummy", 0, "dummy"
+        ).getList();
+
+        List<DrugStore> drugStoreList = new DrugStore(
+                "dummy", List.of("dummy")
+        ).getList();
+
+        System.out.println("===== chap.21 Stream API step up ======");
+        System.out.println("----- exercise.01 -----");
+
+        System.out.println(healthFoodList.stream()
+                .anyMatch(healthFood -> healthFood.category().equals("Energy"))
+                ? "Energy category exists"
+                : "Energy category does not exist");
     }
 
 }
